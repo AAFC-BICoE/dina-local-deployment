d43be130a736c7f510f7e0fa1a43524ed8d3a990
Christian Gendreau <christiangendreau@gmail.com>
2020-05-01 14:49:54 -0400

    Support #19195 Add initial content for dina-local-deployment WIP, working with object-store-api but won't work with more than 1 module for now.

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-rm .env
+# start with a fresh .env
+cat base.env > .env
 
-# $1 = folder, $2 environment variable prefix
+# $1 = folder of the module
 copyDockerComposeFiles()
 {
     cp $1/local/docker-compose.yml.example $1/docker-compose.yml
-    cp $1/local/.env.example $1/.env
-    echo $2"_ROOT_DIR="$1 >> .env
+    cp $1/local/*.env $1/
+    # won't work with more than 1 module. Prefix it ?
+    echo "PATH_FROM_COMPOSE="./$1 >> .env
 }
 
-copyDockerComposeFiles object-store-api OS
+copyDockerComposeFiles object-store-api
 #copyDockerComposeFiles agent-api
-
-cat base.env >> .env
-cat object-store-api/.env >> .env
