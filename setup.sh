ab912e81ab8bb0e31b9dae672e56f3ae1be192be
xuemeigan <xuemei.gan@canada.ca>
2020-12-02 21:40:00 -0500

    Support 21061 Add Seqdb API to dina local deployment - Update .env to include the seqdb api prefix - Add seqdb api service section to docker-compose.local.yml -Update setup.sh to include Seqdb API section

--- setup.sh
+++ setup.sh
@@ -14,3 +14,4 @@ copyDockerComposeFiles()
 copyDockerComposeFiles object-store-api BASE_PATH_TO_OBJECT_STORE
 copyDockerComposeFiles agent-api BASE_PATH_TO_AGENT
 copyDockerComposeFiles dina-user-api BASE_PATH_TO_USER
+copyDockerComposeFiles seqdb-api BASE_PATH_TO_SEQDB
