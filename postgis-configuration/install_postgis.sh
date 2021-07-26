99bb3e8282997c17e3bea37a0cd97a05d9e91680
Jkeyuk <jonkero2@gmail.com>
2021-07-26 10:40:32 -0400

    23533_add_postgis_to_local_deployment
    add install_postgis.sh to install packages only

---
+++ postgis-configuration/install_postgis.sh
@@ -0,0 +1,11 @@
+# install PostGis packages, extensions must be installed separately
+
+export POSTGIS_MAJOR=3
+export POSTGIS_VERSION=3.1.2+dfsg-1~exp2.pgdg100+1
+
+apt-get update \
+      && apt-cache showpkg postgresql-$PG_MAJOR-postgis-$POSTGIS_MAJOR \
+      && apt-get install -y --no-install-recommends \
+           postgresql-$PG_MAJOR-postgis-$POSTGIS_MAJOR=$POSTGIS_VERSION \
+           postgresql-$PG_MAJOR-postgis-$POSTGIS_MAJOR-scripts=$POSTGIS_VERSION \
+      && rm -rf /var/lib/apt/lists/*
