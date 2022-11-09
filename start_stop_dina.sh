4a437a781e76cc0f23a6e383c6e64745300bc08a
Christian Gendreau <christiangendreau@gmail.com>
2022-11-09 14:02:12 -0500

    Changed default modules in start_stop script

--- start_stop_dina.sh
+++ start_stop_dina.sh
@@ -1,15 +1,15 @@
 DINA_MODULES=()
 DINA_MODULES+=('object_store_api')
-DINA_MODULES+=('search_api')
-DINA_MODULES+=('seqdb_api')
-DINA_MODULES+=('report_label_api')
+#DINA_MODULES+=('search_api')
+#DINA_MODULES+=('seqdb_api')
+#DINA_MODULES+=('report_label_api')
 
 #DINA_MODULES+=('kibana')
 #DINA_MODULES+=('prometheus')
 
 printf -v module_arr '%s,' "${DINA_MODULES[@]}"
 
-# remove last comma and export he variable for docker compose
+# remove last comma and export the variable for docker compose
 export COMPOSE_PROFILES=$(echo ${module_arr%,})
 
 echo "Using the following profile(s): $COMPOSE_PROFILES"
