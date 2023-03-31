26340b79c93856f6c946f66741d3cb379499be17
Brandon Andre <andr0284@algonquinlive.com>
2023-03-31 13:21:50 -0400

    Support #30471 - Add API debugging support
    - Added config support to the start_stop_dina script, so you can turn on/off configurations and overrides.
    - Created a new docker-compose.debug.yml file which enables java debugging to be attached and also logging.
    - Created environment variables for the ports.
    - Updated documentation for VSCode debugging

--- start_stop_dina.sh
+++ start_stop_dina.sh
@@ -1,3 +1,4 @@
+# DINA Modules to activate
 DINA_MODULES=()
 DINA_MODULES+=('user_api')
 #DINA_MODULES+=('object_store_api')
@@ -10,13 +11,35 @@ DINA_MODULES+=('user_api')
 #DINA_MODULES+=('kibana')
 #DINA_MODULES+=('prometheus')
 
-printf -v module_arr '%s,' "${DINA_MODULES[@]}"
+# DINA Configurations
+DINA_CONFIGS=()
+DINA_CONFIGS+=('docker-compose.base.yml')
+DINA_CONFIGS+=('docker-compose.local.yml')
+#DINA_CONFIGS+=('docker-compose.dev.yml')
+#DINA_CONFIGS+=('docker-compose.debug.yml')
+#DINA_CONFIGS+=('message-producing-override/docker-compose.override.messageProducer.yml')
+#DINA_CONFIGS+=('persistence-override/docker-compose.override.persistence.yml')
 
-# remove last comma and export the variable for docker compose
-export COMPOSE_PROFILES=$(echo ${module_arr%,})
+# Convert arrays to comma-separated strings
+printf -v module_arr '%s, ' "${DINA_MODULES[@]}"
+printf -v config_arr '%s, ' "${DINA_CONFIGS[@]}"
 
-echo "Using the following profile(s): $COMPOSE_PROFILES"
+# Remove trailing comma and export the variables for docker-compose
+export COMPOSE_PROFILES=$(echo ${module_arr%, })
 
-# pass all the arguments to docker compose
-docker compose -f docker-compose.base.yml -f docker-compose.local.yml $@
+# Remove training commas to display the compose configs to be applied.
+COMPOSE_CONFIGS=$(echo ${config_arr%, })
 
+# Print the profiles and configs being used
+YELLOW_COLOR_CODE="\033[33m"
+WHITE_COLOR_CODE="\033[0m"
+echo -e "${YELLOW_COLOR_CODE}Using the following profile(s):${WHITE_COLOR_CODE} $COMPOSE_PROFILES"
+echo -e "${YELLOW_COLOR_CODE}Using the following config(s):${WHITE_COLOR_CODE} $COMPOSE_CONFIGS"
+
+# Append -f to each config for use in docker-compose
+for i in "${!DINA_CONFIGS[@]}"; do
+  DINA_CONFIGS[$i]="-f ${DINA_CONFIGS[$i]}"
+done
+
+# Run docker-compose with the profiles and configs
+docker compose ${DINA_CONFIGS[@]} $@
