6f6a8027215043333b6013be5c99643de3283323
nazirlkc <nazirlkc@gmail.com>
2026-01-20 13:21:33 -0500

    Fixed ES8 entrypoint script if certs exist

--- elasticsearch8/entrypoint.sh
+++ elasticsearch8/entrypoint.sh
@@ -2,14 +2,13 @@
 
 if [ -f /usr/share/elasticsearch/config/certs/ca.crt ] && [ -f /usr/share/elasticsearch/config/certs/elastic-certificates.p12 ]; then
   echo '✓ Certificates exist'
-  exit 0
+else
+  echo 'Generating certificates...'
+  elasticsearch-certutil ca --out /usr/share/elasticsearch/config/certs/ca.crt --pass ''
+  elasticsearch-certutil cert --ca /usr/share/elasticsearch/config/certs/ca.crt --ca-pass '' --out /usr/share/elasticsearch/config/certs/elastic-certificates.p12 --pass ''
+  chmod 644 /usr/share/elasticsearch/config/certs/*
+  echo '✓ Done'
 fi
-
-echo 'Generating certificates...'
-elasticsearch-certutil ca --out /usr/share/elasticsearch/config/certs/ca.crt --pass ''
-elasticsearch-certutil cert --ca /usr/share/elasticsearch/config/certs/ca.crt --ca-pass '' --out /usr/share/elasticsearch/config/certs/elastic-certificates.p12 --pass ''
-chmod 644 /usr/share/elasticsearch/config/certs/*
-echo '✓ Done'
 
 # Call the original entrypoint
 /usr/local/bin/docker-entrypoint.sh eswrapper
