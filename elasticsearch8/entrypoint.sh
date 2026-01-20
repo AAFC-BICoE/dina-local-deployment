554df506cc402532eae5aebb375d523c7aae65da
nazirlkc <nazirlkc@gmail.com>
2026-01-20 13:15:32 -0500

    Generate certificates for ES8

---
+++ elasticsearch8/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -f /usr/share/elasticsearch/config/certs/ca.crt ] && [ -f /usr/share/elasticsearch/config/certs/elastic-certificates.p12 ]; then
+  echo '✓ Certificates exist'
+  exit 0
+fi
+
+echo 'Generating certificates...'
+elasticsearch-certutil ca --out /usr/share/elasticsearch/config/certs/ca.crt --pass ''
+elasticsearch-certutil cert --ca /usr/share/elasticsearch/config/certs/ca.crt --ca-pass '' --out /usr/share/elasticsearch/config/certs/elastic-certificates.p12 --pass ''
+chmod 644 /usr/share/elasticsearch/config/certs/*
+echo '✓ Done'
+
+# Call the original entrypoint
+/usr/local/bin/docker-entrypoint.sh eswrapper
