f4d08b74057f33db4b262100344283e822f6f3b8
Christian Gendreau <christiangendreau@gmail.com>
2026-05-20 14:41:17 -0400

    Added working p12 configs

--- elasticsearch/entrypoint.sh
+++ elasticsearch/entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-if [ -f /usr/share/elasticsearch/config/certs/ca.crt ] && [ -f /usr/share/elasticsearch/config/certs/elastic-certificates.p12 ]; then
+if [ -f /usr/share/elasticsearch/config/certs/ca.p12 ] && [ -f /usr/share/elasticsearch/config/certs/elastic-certificates.p12 ]; then
   echo '✓ Certificates exist'
 else
   echo 'Generating certificates...'
-  elasticsearch-certutil ca --out /usr/share/elasticsearch/config/certs/ca.crt --pass ''
-  elasticsearch-certutil cert --ca /usr/share/elasticsearch/config/certs/ca.crt --ca-pass '' --out /usr/share/elasticsearch/config/certs/elastic-certificates.p12 --pass ''
-  chmod 644 /usr/share/elasticsearch/config/certs/*
+  elasticsearch-certutil ca --out /usr/share/elasticsearch/config/certs/ca.p12 --pass ''
+  elasticsearch-certutil cert --name elasticsearch-dina --ca /usr/share/elasticsearch/config/certs/ca.p12 --ca-pass '' --out /usr/share/elasticsearch/config/certs/elastic-certificates.p12 --pass ''
   echo '✓ Done'
 fi
 
