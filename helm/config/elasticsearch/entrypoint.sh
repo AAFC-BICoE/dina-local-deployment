e709b1af099eb06f6277ba1986899bdaadb2bca6
nazirlkc <nazirlkc@gmail.com>
2026-06-02 16:41:20 -0400

    ES8 helm fixes
    * created shared PVC and mounted it to search-api services
    * updated dns value for ES entrypoint script
    * updated config for export-api

--- helm/config/elasticsearch/entrypoint.sh
+++ helm/config/elasticsearch/entrypoint.sh
@@ -7,7 +7,7 @@ else
   elasticsearch-certutil ca --pem --days 3650 --out /usr/share/elasticsearch/config/certs/elastic-ca.zip
   unzip -d /usr/share/elasticsearch/config/certs /usr/share/elasticsearch/config/certs/elastic-ca.zip
   
-  elasticsearch-certutil cert --name elastic-certs --ca-cert /usr/share/elasticsearch/config/certs/ca/ca.crt --ca-key /usr/share/elasticsearch/config/certs/ca/ca.key --pem --dns 'elasticsearch-dina' --days 3650 --out /usr/share/elasticsearch/config/certs/elastic-certs.zip
+  elasticsearch-certutil cert --name elastic-certs --ca-cert /usr/share/elasticsearch/config/certs/ca/ca.crt --ca-key /usr/share/elasticsearch/config/certs/ca/ca.key --pem --dns 'elasticsearch-service' --days 3650 --out /usr/share/elasticsearch/config/certs/elastic-certs.zip
 
   elasticsearch-certutil cert --name elasticsearch-node-dina --ca-cert /usr/share/elasticsearch/config/certs/ca/ca.crt --ca-key /usr/share/elasticsearch/config/certs/ca/ca.key --out /usr/share/elasticsearch/config/certs/elastic-node-certificates.p12 --pass ''
   
